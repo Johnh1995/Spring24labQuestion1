741e29caf78294d1c94772e7f490126ea07fd7c5
Johnh1995 <johnnyhayess@gmail.com>
2024-04-20 10:26:02 +0000

    Lab1 mbed OS error
    Signed-off-by: Johnh1995 <johnnyhayess@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "mbed.h"
+#include <cstdio>
+
+InterruptIn buttn(PC_13);
+
+void buttn_fall(){
+    printf("button pressed!!\r\n");
+}
+// main() runs in its own thread in the OS
+int main()
+{
+    printf("This is mbed os v%d \r\n" , MBED_VERSION);
+
+     buttn.fall(&buttn_fall);
+    while (true) {
+
+    }
+}
+
